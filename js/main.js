4e9be25d35bf05680d8add75323cc97cad22203e
Danila Loginov <loginovdanila@gmail.com>
2017-09-14 20:37:19 +0300

    Device requesting and connection separated, added disconnection handling

--- js/main.js
+++ js/main.js
@@ -4,42 +4,69 @@ const CHARACTERISTIC_UUID = 0xFFE1;
 let connectionButton = document.getElementById('connection');
 let consoleDiv = document.getElementById('console');
 
+let bluetoothDevice = null;
+let bluetoothCharacteristic = null;
+
 connectionButton.addEventListener('click', function(event) {
-  connect();
+  return (bluetoothDevice ? Promise.resolve() : requestBluetoothDevice()).
+      then(connectDeviceAndCacheCharacteristic).
+      catch(error => log(error));
 });
 
-function connect() {
+function requestBluetoothDevice() {
+  log('Requesting bluetooth device...');
+
   return navigator.bluetooth.requestDevice({
     filters: [{services: [SERVICE_UUID]}],
   }).
       then(device => {
-        log('Device connected');
-        console.log(device);
-        return device.gatt.connect();
-      }).
+        log('Bluetooth device found');
+
+        bluetoothDevice = device;
+        bluetoothDevice.addEventListener('gattserverdisconnected',
+            handleDisconnection);
+
+        return bluetoothDevice;
+      });
+}
+
+function connectDeviceAndCacheCharacteristic() {
+  if (bluetoothDevice.gatt.connected && bluetoothCharacteristic) {
+    return Promise.resolve();
+  }
+
+  log('Connecting to GATT server...');
+
+  return bluetoothDevice.gatt.connect().
       then(server => {
-        log('Server connected');
-        console.log(server);
+        log('GATT server connected', 'Getting service...');
+
         return server.getPrimaryService(SERVICE_UUID);
       }).
       then(service => {
-        log('Service found');
-        console.log(service);
+        log('Service found', 'Getting characteristic...');
+
         return service.getCharacteristic(CHARACTERISTIC_UUID);
       }).
       then(characteristic => {
         log('Characteristic found');
-        console.log(characteristic);
-        return characteristic;
-      }).
-      catch(error => {
-        log(error);
+
+        bluetoothCharacteristic = characteristic;
+
+        return bluetoothCharacteristic;
       });
 }
 
-function log(text) {
-  let html = text + '<br>';
+function log(...messages) {
+  let html = messages.join('<br>') + '<br>';
 
-  console.log(text);
+  messages.forEach(message => console.log(message));
   consoleDiv.insertAdjacentHTML('beforeend', html);
+}
+
+function handleDisconnection() {
+  log('Bluetooth device disconnected');
+
+  return connectDeviceAndCacheCharacteristic().
+      catch(error => log(error));
 }
