8fcaf1dad16a8ca090604fadfbec0f9eb3ac7f3a
Danila Loginov <loginovdanila@gmail.com>
2017-09-19 21:07:14 +0300

    `BluetoothApplication` class removed, min logic implemented functionally instead

--- js/main.js
+++ js/main.js
@@ -1,46 +1,48 @@
-class BluetoothApplication extends BluetoothConnection {
-  constructor(serviceUuid, characteristicUuid) {
-    super(serviceUuid, characteristicUuid);
-
-    this._consoleContainer = document.getElementById('console');
-    this._terminalContainer = document.getElementById('terminal');
-  }
-
-  send(data) {
-    if (super.send(data)) {
-      this._logToTerminal('< ' + data);
-    }
-  }
-
-  receive(data) {
-    this._logToTerminal('> ' + data);
-  }
-
-  _log(...messages) {
-    super._log(...messages);
-
-    let html = messages.join('<br>') + '<br>';
-    this._consoleContainer.insertAdjacentHTML('beforeend', html);
-  }
-
-  _logToTerminal(message) {
-    let html = message + '<br>';
-    this._terminalContainer.insertAdjacentHTML('beforeend', html);
-  }
-}
-
-let connection = new BluetoothApplication(0xFFE0, 0xFFE1);
-
+// UI elements
 let connectButton = document.getElementById('connect');
 let disconnectButton = document.getElementById('disconnect');
 let inputField = document.getElementById('input');
 let sendButton = document.getElementById('send');
 
+let terminalContainer = document.getElementById('terminal');
+let consoleContainer = document.getElementById('console');
+
+// Helpers
+function logToTerminal(message) {
+  let html = message + '<br>';
+  terminalContainer.insertAdjacentHTML('beforeend', html);
+}
+
+// Create bluetooth connection instance
+let connection = new BluetoothConnection(0xFFE0, 0xFFE1);
+
+// Implement own send function to log outcoming data to the terminal element
+function send(data) {
+  if (connection.send(data)) {
+    logToTerminal('< ' + data);
+  }
+}
+
+// Override receive method to log incoming data to the terminal element
+connection.receive = function(data) {
+  logToTerminal('> ' + data);
+};
+
+// Override connection's log method to output messages to the console element
+connection._log = function(...messages) {
+  // We cannot use `super._log()` here
+  messages.forEach(message => console.log(message));
+
+  let html = messages.join('<br>') + '<br>';
+  consoleContainer.insertAdjacentHTML('beforeend', html);
+};
+
+// Bind event listeners to the UI elements
 connectButton.addEventListener('click', () => connection.connect());
 
 disconnectButton.addEventListener('click', () => connection.disconnect());
 
 sendButton.addEventListener('click', () => {
-  connection.send(inputField.value);
+  send(inputField.value);
   inputField.value = '';
 });
