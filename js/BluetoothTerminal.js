fbed6fa97c2c1f1e554bfbf4580b031afa0e4da7
Tom Himanen <tom.himanen@gmail.com>
2020-03-10 12:23:05 +0200

    Enables detection of other kind of BT modules

--- js/BluetoothTerminal.js
+++ js/BluetoothTerminal.js
@@ -242,7 +242,8 @@ class BluetoothTerminal {
     this._log('Requesting bluetooth device...');
 
     return navigator.bluetooth.requestDevice({
-      filters: [{services: [this._serviceUuid]}],
+      //filters: [{services: [this._serviceUuid]}],
+      acceptAllDevices: true
     }).
         then((device) => {
           this._log('"' + device.name + '" bluetooth device selected');
